60a393d5e42c5215d37d5ff41a1b767208227dc3
youngjin34 <epik35@naver.com>
2024-11-05 13:33:09 +0900

    feat, add: 지도 추가 중

--- js/loadData.js
+++ js/loadData.js
@@ -4,14 +4,26 @@ function loadMatzip() {
   const existingMatzip = JSON.parse(localStorage.getItem("boards")) || [];
   console.log(existingMatzip);
 
+  const latestMatzip = existingMatzip.slice(-6);
+
   matzipList.innerHTML = "";
 
-  existingMatzip.forEach((matzip) => {
+  latestMatzip.forEach((matzip) => {
     const listItem = document.createElement("div");
+    const randomNum = Math.floor(Math.random() * 7) + 1;
+
     listItem.innerHTML = `
-      <span>작성자: ${matzip.userId}</span>
-      <span>제목: ${matzip.title}</span>
-      <span>${matzip.date}</span>
+      ${
+        matzip.image
+          ? `<img class="board-image" src="${matzip.image}" alt="게시글 이미지" />`
+          : `<img class="board-image" src="../images/${randomNum}.png"`
+      }
+      <div>
+        <span>작성자: ${matzip.userId}</span>
+        <span>제목: ${matzip.title}</span>
+        <span class="board-content">내용: ${matzip.content}</span>
+        <span>${matzip.date}</span>
+      </div>
     `;
     listItem.classList.add("matzip-list");
 
